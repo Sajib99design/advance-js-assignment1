0b648b9ac23e4d25481709d513797307fb0beb6c
Sajib99design <sajib99design@gmail.com>
2025-06-30 00:53:27 +0600

    function

--- js/script.js
+++ js/script.js
@@ -10,3 +10,36 @@ addButton.addEventListener('click', () => {
         input.value = "";
     }
 });
+
+
+// add todo methods
+function addTodoItem(text) {
+    const li = document.createElement('li');
+    li.className = "flex justify-between items-center bg-gray-200 px-4 py-2 rounded";
+
+    const span = document.createElement('span');
+    span.textContent = text;
+
+    const buttons = document.createElement('div');
+    buttons.className = "space-x-2";
+
+    //   edit btn
+    const editButton = document.createElement('button');
+    editButton.textContent = "Edit";
+    editButton.className = "text-yellow-600 hover:underline cursor-pointer";
+    editButton.onclick = () => editTodoItem(span, editButton);
+
+    //   delete btn
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = "Delete";
+    deleteBtn.className = "text-red-600 hover:underline cursor-pointer";
+    deleteBtn.onclick = () => li.remove();
+
+    buttons.appendChild(editButton);
+    buttons.appendChild(deleteBtn);
+
+    li.appendChild(span);
+    li.appendChild(buttons);
+
+    todoList.appendChild(li);
+}
